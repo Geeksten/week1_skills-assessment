7eb51d1ecf9c9022c0c1cff4b0bdc50102fdd242
Geeksten <temozex@gmail.com>
2015-09-27 15:56:44 -0700

    added average py file

---
+++ average.py
@@ -0,0 +1,16 @@
+def average(number_list):
+    """Return the average (mean) of the list of numbers given.
+
+        >>> average([2, 12, 3])
+        5.666666666666667
+
+    There is no defined answer if the list given is empty. It's fine if
+    this raises an error when given an empty list.
+    """
+    list_sum = 0
+    for num in number_list:
+        list_sum += num
+    average_nums = (float(list_sum)/(len(number_list)))
+    return average_nums
+
+print average([2, 12, 3])
--- skills.py
+++ skills.py
@@ -261,8 +261,13 @@ def average(number_list):
     There is no defined answer if the list given is empty. It's fine if
     this raises an error when given an empty list.
     """
+    list_sum = 0
+    for num in number_list:
+        list_sum += num
+    average_nums = (float(list_sum)/(len(number_list)))
+    return average_nums
 
-    return 0
+print average([2, 12, 3])
 
 
 #############################################################################
