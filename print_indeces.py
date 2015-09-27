1bb47b8cb6b0f521144804fc7956190ab4cd0223
Geeksten <temozex@gmail.com>
2015-09-27 15:32:01 -0700

    added print_indeced py file

---
+++ print_indeces.py
@@ -0,0 +1,25 @@
+def print_indeces(my_list):
+    """Print the index of each list item, followed by the item itself.
+    Do this without using a counting variable. I.e. don't do something
+    like this:
+
+    count = 0
+    for item in list:
+        print count
+        count = count + 1
+
+    Output should look like this:
+
+    >>> print_indeces(["Toyota", "Jeep", "Volvo"])
+    0 Toyota
+    1 Jeep
+    2 Volvo
+
+    """
+
+    index = 0
+    for item in my_list:
+        print("{}  {}").format(index, item)
+        index += 1
+
+print_indeces(["Toyota", "Jeep", "Volvo"])
