0fc03d87bb0d9ac2b0b4e90cdb0fdd1dc8dda622
Geeksten <temozex@gmail.com>
2015-09-27 13:37:17 -0700

    added the word lengths py file, with function word-lengths. Returns length of words in input list

---
+++ word_lengths.py
@@ -0,0 +1,18 @@
+def word_lengths(word_list):
+    """Return the length of words in the input list.
+
+        >>> word_lengths(["hello", "hey", "hello", "spam"])
+        [5, 3, 5, 4]
+
+    """
+    index = 0
+    length_of_words_list = []
+    #while word_list[index] < len(word_list):
+    for word in word_list[:]:
+        the_word_length = len(word)
+        length_of_words_list.append(the_word_length)
+
+    #index = index + 1
+    return length_of_words_list
+    #return []
+print word_lengths(["hello", "hey", "hello", "spam"])
