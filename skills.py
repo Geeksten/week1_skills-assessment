b3a7b736900f4451841ace7fdb8ed3ed2792c79c
Geeksten <temozex@gmail.com>
2015-09-27 18:13:55 -0700

    advanced_join_strings py file and advanced_join_strings function to skills dot py

--- skills.py
+++ skills.py
@@ -275,30 +275,28 @@ print average([2, 12, 3])
 #print average([])
 
 
+def advanced_join_strings(list_of_words):
+    """Return a single string with each word from the input list
+    separated by a comma.
 
-#############################################################################
-# END OF SKILLS TEST: You can stop here, or read on to work on advanced problem.
+        >>> advanced_join_strings(["Labrador", "Poodle", "French Bulldog"])
+        'Labrador, Poodle, French Bulldog'
 
-# Uncomment the function below to work on the advanced problem.
-# Tip: To comment or uncomment blocks of code, highlight what you want to
-#    comment or uncomment, and then CMD+"/" or CTRL-"/"
+    If there's only one thing in the list, it should return just that
+    thing, of course:
 
-# def advanced_join_strings(list_of_words):
-#     """Return a single string with each word from the input list
-#     separated by a comma.
+        >>> advanced_join_strings(["Pretzel"])
+        'Pretzel'
 
-#         >>> advanced_join_strings(["Labrador", "Poodle", "French Bulldog"])
-#         'Labrador, Poodle, French Bulldog'
+    """
 
-#     If there's only one thing in the list, it should return just that
-#     thing, of course:
+#    result_string = ''
+    return ', '.join(list_of_words)
+    #    result_string += element
+    #return result_string
 
-#         >>> advanced_join_strings(["Pretzel"])
-#         'Pretzel'
-
-#     """
-
-#     return ""
+print advanced_join_strings(["Labrador", "Poodle", "French Bulldog"])
+print advanced_join_strings(["Pretzel"])
 
 # END OF ASSIGNMENT: You can ignore everything below.
 ##############################################################################
